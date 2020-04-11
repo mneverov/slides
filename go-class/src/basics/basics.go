bb4b340d44b4300fc96dc6d8cc9756141fff045f
Maxim Neverov <maxim.neverov@zalando.de>
2020-04-11 10:22:00 +0200

    Use UTF-8 example from go blog

--- go-class/src/basics/basics.go
+++ go-class/src/basics/basics.go
@@ -9,9 +9,10 @@ func main() {
 	x = 3.0 + y
 	fmt.Println("x:", x, ", y:", y, ", z:", z)
 
-	const str string = "this is a 日本語 string\n"
-	var ch byte = str[12]
-	r := []rune(str)
-	fmt.Printf("ch: %q, rune: %q\n", ch, r[12])
-	fmt.Printf("ch  : %[1]q/%[1]v/%016[1]b\nrune: %[2]q/%[2]v/%016[2]b\n", ch, r[12])
+	const nihongo string = "日本語"
+
+	for index, runeValue := range nihongo {
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	}
+
 }
